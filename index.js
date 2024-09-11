2c924b2a331892c561b5503816f6dcf9357c4c73
Vitor Emanoel da Silva Nogueira <139004643+VitorEmanoelNogueira@users.noreply.github.com>
2024-09-11 18:09:58 -0300

    inicia o menu da aplicação

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
-let meta = {
-    value: 'ler um livro por mês',
-    checked: false,
-    isChecked : () => {
+const { select } = require('@inquirer/prompts')
 
+const start = async () => {
+
+    while(true){
+        
+        const opcao = await select({
+            message: 'Menu >',
+            choices: [
+                {
+                    name: 'Cadastrar meta',
+                    value: 'cadastrar'
+                },
+                {
+                    name: 'Listar metas',
+                    value: 'listar'
+                },
+                {
+                    name: 'Sair',
+                    value: 'sair'
+                }
+            ]
+        })
+
+        switch(opcao){
+            case 'cadastrar':
+                console.log('ae fi')
+                break;
+            case 'listar':
+                
+                break;
+            case 'sair':
+                console.log('Até a próxima!')
+                return
+        }
     }
 }
 
-console.log();
-meta.isChecked()
-
-const criarMeta = () => {}
+start()
