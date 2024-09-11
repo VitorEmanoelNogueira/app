f8e19e5bcbef67ea0655f44fd14f91abfd750fe0
Vitor Emanoel da Silva Nogueira <139004643+VitorEmanoelNogueira@users.noreply.github.com>
2024-09-11 18:33:12 -0300

    listar metas

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { select, input } = require('@inquirer/prompts')
+const { select, input, checkbox } = require('@inquirer/prompts')
 
 let meta = {
     value: 'Tomar 3L de água por dia',
@@ -18,7 +18,31 @@ const cadastrarMeta = async() => {
     metas.push({value: meta, checked: false});
 }
 
+const listarMetas = async () => {
+    const respostas = await checkbox({
+        message: 'Use as setas para mudar de meta, o espaço para marcar ou desmarcar e o Enter para finalizar essa etapa',
+         choices: [...metas]
+    })
 
+    if(respostas.length == 0){
+        console.log('Nenhuma meta selecionada!');
+        return
+    }
+
+    metas.forEach((m) => {
+        m.checked = false;
+    })
+
+    respostas.forEach((resposta) => {
+        const meta = metas.find((n) => {
+            return n.value == resposta;
+        })
+
+        meta.checked = true;
+    })
+
+    console.log('Meta(s) marcada(s) como concluída(s)');
+}
 
 const start = async () => {
 
@@ -48,10 +72,10 @@ const start = async () => {
                 console.log(metas);
                 break;
             case 'listar':
-                
+                await listarMetas();
                 break;
             case 'sair':
-                console.log('Até a próxima!')
+                console.log('Até a próxima!');
                 return
         }
     }
