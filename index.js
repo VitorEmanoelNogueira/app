f18cb51f092e6c5e786c448b2c1ea79acff5a0b8
Vitor Emanoel da Silva Nogueira <139004643+VitorEmanoelNogueira@users.noreply.github.com>
2024-09-12 08:33:12 -0300

    adiciona metas abertas

--- index.js
+++ index.js
@@ -60,6 +60,22 @@ const metasRealizadas = async () => {
     })
 }
 
+const metasAbertas = async () => {
+    const abertas = metas.filter((meta) => {
+        return !meta.checked;
+    })
+
+    if(abertas.length == 0){
+        console.log('Não existem metas abertas! :)');
+        return
+    }
+
+    await select({
+        message: `Metas abertas: ${abertas.length}`,
+        choices: [...abertas]
+    })
+}
+
 
 const start = async () => {
 
@@ -81,6 +97,10 @@ const start = async () => {
                     value: 'realizadas'
                 },
                 {
+                    name: 'Metas abertas',
+                    value: 'abertas'
+                },
+                {
                     name: 'Sair',
                     value: 'sair'
                 }
@@ -98,6 +118,9 @@ const start = async () => {
             case 'realizadas':
                 await metasRealizadas();
                 break;
+                case 'abertas':
+                    await metasAbertas();
+                    break;
             case 'sair':
                 console.log('Até a próxima!');
                 return
